d3336de046b77e40c7ef66ffbe5f7fde2796562a
sungter <eroomeuzz@naver.com>
2025-03-19 17:12:40 +0900

    파일 업로드 기능 수업, 구현

--- src/components/practice/UploadTest.jsx
+++ src/components/practice/UploadTest.jsx
@@ -10,15 +10,16 @@ const UploadTest = () => {
   //자바로 데이터를 전달할 때, 문자 뿐 아니라 파일 데이터도 가져간다는 것을 설정
   const fileConfig = {header : {'Content-Type' : 'multipart/form-data'}};
 
-  //첨부파일 데이터를 자바로 전달하기 위해서는 FormData() 객체를 사용해야한다.
-  //form 데이터 객체 생성 : 첨부파일, input 태그 등의 모든 데이터를 
-  //                       자바로 가져갈 수 있게 변환해주는 객체
-  const form = new FormData();
-  form.append('name' , 'hong');
-  form.append('age' , 20);
-  form.append('firstFile', firstFile);
-
   const sendFile = () => {
+    //첨부파일 데이터를 자바로 전달하기 위해서는 FormData() 객체를 사용해야한다.
+    //form 데이터 객체 생성 : 첨부파일, input 태그 등의 모든 데이터를 
+    //                       자바로 가져갈 수 있게 변환해주는 객체
+    const form = new FormData();
+    form.append('bookName' , 'hong');
+    form.append('bookPrice' , 20);
+    form.append('firstFile', firstFile);
+
+
     //post() 메서드의 세번째 매개변수로 fileConfig를 전달 
     //*(파일 첨부 java에 보내기 위해서 필수적으로 사용해야되는 정의된 문법)
     axios.post(
@@ -42,8 +43,8 @@ const UploadTest = () => {
           console.log(e.target.files[0]);
 
           //파일을 선택할 때 마다 선택한 파일을 firstFile에 저장한다.
-          setFirstFile(e.target.files[0]);
-        }} 
+          setFirstFile(e.target.files[0]);       
+        }}
       />
 
       <button type='button' onClick={() => {sendFile()}}>파일 전송 1</button>
