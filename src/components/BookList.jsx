6fd1882ec1d9c8b14dd883d96e24133505cc5492
sungter <eroomeuzz@naver.com>
2025-03-25 17:21:21 +0900

    20250325

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -6,7 +6,7 @@ import { useParams } from 'react-router-dom';
 const BookList = () => {
 
   //책 리스트 가져와 저장할 변수
-  const [BookList, setBookList] = useState([]);
+  const [bookList, setBookList] = useState([]);
 
   //책 리스트 불러오는 함수
   useEffect(() => {
@@ -14,12 +14,13 @@ const BookList = () => {
         .then(res => setBookList(res.data))
         .catch(error => console.log(error))
   }, []);
+  console.log(bookList)
 
 
   return (
     <div className={styles.book_list}>
       {
-        BookList.map((book, i) => {
+        bookList.map((book, i) => {
           return(
             <div key={i}>
               <div>이미지</div>
